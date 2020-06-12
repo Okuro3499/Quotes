ab126295638959558392512c5e911a7e360fd6af
Gideon Okuro <gideonollonde@gmail.com>
2020-06-12 19:16:53 +0300

    create upvote and downvote functionality

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -7,9 +7,25 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  constructor() { }
 
   @Input() quote: Quote;
-  constructor() { }
+  @Output() iscomplete = new EventEmitter<boolean>();
+
+  downvote = 0;
+  upvote = 0;
+
+  quoteDelete(complete: boolean) {
+    this.iscomplete.emit(complete);
+  }
+
+  voteDown() {
+    this.downvote = this.downvote + 1;
+  }
+
+  voteUp() {
+    this.upvote = this.upvote + 1;
+  }
 
   ngOnInit(): void {
   }
