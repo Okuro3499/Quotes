1ab6a0b6f7afa2c8c1c3ccccdeed026bb1cb8faf
Gideon Okuro <gideonollonde@gmail.com>
2020-06-12 17:59:25 +0300

    add a description to our quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes: Quote[] = [
-    { id: 1, name: 'Love For All, Hatred For None.' },
-    { id: 2, name: 'Die with memories, not dreams' },
-    { id: 3, name: 'Simplicity is the ultimate sophistication.' },
+    { id: 1, name: 'Love For All, Hatred For None.', description: 'by Khalifatul Masih III' },
+    { id: 2, name: 'Die with memories, not dreams', description: 'by T.S Eliot' },
+    { id: 3, name: 'Simplicity is the ultimate sophistication.', description: 'by Leonardo da Vinci' },
   ];
 }
