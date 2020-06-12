76116dc5cd45779bfafa14370b1d6b46443a5bd3
Gideon Okuro <gideonollonde@gmail.com>
2020-06-12 22:02:24 +0300

    update Quotes array to add a posted date

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Love For All, Hatred For None.', 'by Khalifatul Masih III'),
-    new Quote(2, 'Die with memories, not dreams', 'by T.S Eliot'),
-    new Quote(3, 'Simplicity is the ultimate sophistication.', 'by Leonardo da Vinci'),
+    new Quote(1, 'Love For All, Hatred For None.', 'by Khalifatul Masih III', new Date(2005, 4, 20)),
+    new Quote(2, 'Die with memories, not dreams', 'by T.S Eliot', new Date(2010, 2, 10)),
+    new Quote(3, 'Simplicity is the ultimate sophistication.', 'by Leonardo da Vinci', new Date(1998, 6, 10)),
   ];
 
   toggleDetails(index) {
