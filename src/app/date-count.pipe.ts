9c6cb2d03243d0608633284a6784dbb9676b9fd1
Gideon Okuro <gideonollonde@gmail.com>
2020-06-12 23:04:32 +0300

    define logic to count the number of days since the quote was posted

---
+++ src/app/date-count.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCount'
+})
+export class DateCountPipe implements PipeTransform {
+
+  transform(value: any): number {
+    // tslint:disable-next-line: prefer-const
+    let today: Date = new Date(); // get current date and time
+    // tslint:disable-next-line: prefer-const
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    // tslint:disable-next-line: prefer-const && no-var-keyword
+    var dateDifference = Math.abs(value - todayWithNoTime); // returns value in milliseconds
+    const secondsInDay = 86400; // 60secs * 60mins in an hr * 24hrs in a day
+    // tslint:disable-next-line: prefer-const && no-var-keyword
+    var dateDifferenceSeconds = dateDifference * 0.001; // converts milliseconds to secs
+    // tslint:disable-next-line: prefer-const && no-var-keyword
+    var dateCounter = dateDifferenceSeconds / secondsInDay;
+
+    if (dateCounter >= 1 && value > todayWithNoTime) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
+  }
+}
