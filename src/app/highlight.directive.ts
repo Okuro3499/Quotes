7ca9179963f847efc109dbd8c204bc09b54ab12e
Gideon Okuro <gideonollonde@gmail.com>
2020-06-12 21:46:59 +0300

    fix highlight functionality error

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Host } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'yellow';
-  }
+  constructor(private elem: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter(){
+  this.highlight('#FFFF00');
+}
+@HostListener('mouseleave') onMouseLeave() {
+  this.highlight(null);
+}
+    private highlight(color: string) {
+  this.elem.nativeElement.style.backgroundColor = color;
+}
 }
